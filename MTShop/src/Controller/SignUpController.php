0d0d99e8379f5b3b0aa2668db8f2a7e69d03efd4
matthieuthuetdev <mthuet.pro@gmail.com>
2026-07-19 17:33:05 +0200

    remove duplicate account check from sign up controller

--- MTShop/src/Controller/SignUpController.php
+++ MTShop/src/Controller/SignUpController.php
@@ -4,10 +4,8 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\SignUpType;
-use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -18,41 +16,35 @@ final class SignUpController extends AbstractController
     #[Route('/sign/up', name: 'app_sign_up')]
     public function index(
         Request $request,
-        UserRepository $userRepository,
         EntityManagerInterface $entityManager,
         UserPasswordHasherInterface $passwordHasher,
-    ): Response
-    {
+    ): Response {
         $form = $this->createForm(SignUpType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $email = mb_strtolower((string) $form->get('email')->getData());
 
-            if ($userRepository->findOneBy(['email' => $email]) !== null) {
-                $form->get('email')->addError(new FormError('Cette adresse mail est déjà utilisée.'));
-            } else {
-                $user = new User();
-                $user->setFirstName((string) $form->get('firstName')->getData());
-                $user->setLastName((string) $form->get('lastName')->getData());
-                $user->setEmail($email);
-                $user->setRoles(['ROLE_USER']);
-                $user->setPassword(
-                    $passwordHasher->hashPassword(
-                        $user,
-                        (string) $form->get('password')->getData()
-                    )
-                );
-                $user->setShippingAddress('');
-                $user->setBillingAddress('');
+            $user = new User();
+            $user->setFirstName((string) $form->get('firstName')->getData());
+            $user->setLastName((string) $form->get('lastName')->getData());
+            $user->setEmail($email);
+            $user->setRoles(['ROLE_USER']);
+            $user->setPassword(
+                $passwordHasher->hashPassword(
+                    $user,
+                    (string) $form->get('password')->getData()
+                )
+            );
+            $user->setShippingAddress('');
+            $user->setBillingAddress('');
 
-                $entityManager->persist($user);
-                $entityManager->flush();
+            $entityManager->persist($user);
+            $entityManager->flush();
 
-                $this->addFlash('success', 'Votre compte a bien été créé. Vous pouvez maintenant vous connecter.');
+            $this->addFlash('success', 'Votre compte a bien été créé. Vous pouvez maintenant vous connecter.');
 
-                return $this->redirectToRoute('app_sign_up');
-            }
+            return $this->redirectToRoute('app_sign_up');
         }
 
         return $this->render('sign_up/index.html.twig', [
