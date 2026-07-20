a54ecb1534c7b5da811c2cb1a1359a6ed72c2de8
matthieuthuetdev <mthuet.pro@gmail.com>
2026-07-20 09:20:36 +0200

    add authenticated menu and profile page

--- MTShop/src/Controller/SecurityController.php
+++ MTShop/src/Controller/SecurityController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 final class SecurityController extends AbstractController
@@ -26,5 +27,12 @@ final class SecurityController extends AbstractController
     public function logout(): never
     {
         throw new \LogicException('This method is intercepted by the firewall logout.');
+    }
+
+    #[Route('/profile', name: 'app_profile')]
+    #[IsGranted('ROLE_USER')]
+    public function profile(): Response
+    {
+        return $this->render('security/profile.html.twig');
     }
 }
