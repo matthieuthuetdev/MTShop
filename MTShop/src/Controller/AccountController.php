9f3731ab19cf73a6f66261c6049861f22c6ca9ec
matthieuthuetdev <mthuet.pro@gmail.com>
2026-07-22 10:56:02 +0200

    add email update form

--- MTShop/src/Controller/AccountController.php
+++ MTShop/src/Controller/AccountController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -21,5 +24,48 @@ final class AccountController extends AbstractController
     public function edit(): Response
     {
         return $this->render('account/edit.html.twig');
+    }
+
+    #[Route('/my-account/edit/email', name: 'app_my_account_update_email', methods: ['POST'])]
+    #[IsGranted('ROLE_USER')]
+    public function updateEmail(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        UserRepository $userRepository,
+    ): Response {
+        $token = (string) $request->request->get('_token', '');
+
+        if (!$this->isCsrfTokenValid('account_update_email', $token)) {
+            $this->addFlash('danger', 'Your request could not be verified.');
+
+            return $this->redirectToRoute('app_my_account_edit');
+        }
+
+        $user = $this->getUser();
+        if (null === $user) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $email = mb_strtolower(trim((string) $request->request->get('email', '')));
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $this->addFlash('danger', 'Please enter a valid email address.');
+
+            return $this->redirectToRoute('app_my_account_edit');
+        }
+
+        $existingUser = $userRepository->findOneBy(['email' => $email]);
+        if (null !== $existingUser && $existingUser !== $user) {
+            $this->addFlash('danger', 'This email address is already used.');
+
+            return $this->redirectToRoute('app_my_account_edit');
+        }
+
+        $user->setEmail($email);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Your email address has been updated.');
+
+        return $this->redirectToRoute('app_my_account_edit');
     }
 }
