b4bec8a8d85525c4d20cd15503c9d646ec3b573c
matthieuthuetdev <mthuet.pro@gmail.com>
2026-07-22 22:57:05 +0200

    add two factor authentication flow

--- MTShop/src/Security/LoginFormAuthenticator.php
+++ MTShop/src/Security/LoginFormAuthenticator.php
@@ -2,10 +2,15 @@
 
 namespace App\Security;
 
+use App\Entity\User;
+use App\Service\TwoFactorAuthenticationManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
@@ -21,6 +26,9 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 
     public function __construct(
         private readonly UrlGeneratorInterface $urlGenerator,
+        private readonly TwoFactorAuthenticationManager $twoFactorAuthenticationManager,
+        private readonly EntityManagerInterface $entityManager,
+        private readonly TokenStorageInterface $tokenStorage,
     ) {
     }
 
@@ -47,6 +55,39 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 
     public function onAuthenticationSuccess(Request $request, $token, string $firewallName): ?RedirectResponse
     {
+        $session = $request->getSession();
+        $user = $token->getUser();
+
+        if ($user instanceof UserInterface && $user instanceof User) {
+            $bypassUserId = $session->get('two_factor.bypass_user_id');
+            if ($user->getId() === $bypassUserId) {
+                $session->remove('two_factor.bypass_user_id');
+                $session->remove('two_factor.target_path');
+
+                $targetPath = $this->getTargetPath($session, $firewallName);
+
+                if (null !== $targetPath) {
+                    return new RedirectResponse($targetPath);
+                }
+
+                return new RedirectResponse($this->urlGenerator->generate('app_home_page'));
+            }
+
+            if ($user->isTwoFactorEnabled()) {
+                $code = $this->twoFactorAuthenticationManager->startChallenge($user);
+                $this->entityManager->flush();
+                $this->twoFactorAuthenticationManager->sendCode($user, $code);
+
+                $session->set('two_factor.pending_user_id', $user->getId());
+                $session->remove('two_factor.bypass_user_id');
+
+                $this->tokenStorage->setToken(null);
+                $session->remove(sprintf('_security_%s', $firewallName));
+
+                return new RedirectResponse($this->urlGenerator->generate('app_two_factor'));
+            }
+        }
+
         $targetPath = $this->getTargetPath($request->getSession(), $firewallName);
 
         if (null !== $targetPath) {
