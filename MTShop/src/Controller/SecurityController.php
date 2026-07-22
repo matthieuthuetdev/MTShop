e3b95fcd5282caaa5bb84cc07c2819a3c621a781
matthieuthuetdev <mthuet.pro@gmail.com>
2026-07-22 15:10:12 +0200

    add a debug button to low the system session

--- MTShop/src/Controller/SecurityController.php
+++ MTShop/src/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -26,5 +27,15 @@ final class SecurityController extends AbstractController
     public function logout(): never
     {
         throw new \LogicException('This method is intercepted by the firewall logout.');
+    }
+
+    #[Route('/clear-session', name: 'app_clear_session', methods: ['POST'])]
+    public function clearSession(Request $request): Response
+    {
+        if ($this->isCsrfTokenValid('clear_session', (string) $request->request->get('_token', ''))) {
+            $request->getSession()->invalidate();
+        }
+
+        return $this->redirectToRoute('app_login');
     }
 }
